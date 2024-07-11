2155eb2cb3b515cec1d371bced3b540022810c85
V <lamkencong@gmail.com>
2024-07-11 11:19:13 +0800

    chore: Refactor ERC20Mock contract and add update lib

--- contracts/mock/ERC20Mock.sol
+++ contracts/mock/ERC20Mock.sol
@@ -4,11 +4,13 @@ pragma solidity ^0.8.25;
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract ERC20Mock is ERC20 {
-    constructor() ERC20("ERC20Mock", "ERC20Mock") {
-        _mint(msg.sender, 10000 * 1e18);
-    }
+    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {}
 
     function mint(address _account, uint256 _amount) external {
         _mint(_account, _amount);
+    }
+
+    function mintByETH(address _account, uint256 _eth) external {
+        _mint(_account, _eth * 1 ether);
     }
 }
